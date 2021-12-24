016209d4cc18d2c54a2b30c076f4c8d77a2dcc7a
DwarfPorter <mrvlmor@gmail.com>
2021-12-24 21:49:33 +0300

    Смерджил

--- app/src/main/java/ru/geekbrains/team20211224/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/team20211224/MainActivity.kt
@@ -14,6 +14,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button = findViewById(R.id.button)
-        btn = findViewById(R.id.button)
+        btn = findViewById(R.id.button1)
     }
 }
