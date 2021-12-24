3c74c4e06481ff8cc8623f96c24769a40772756e
VMoLinov <73303695+VMoLinov@users.noreply.github.com>
2021-12-24 21:46:25 +0300

    red btn (#3)

--- app/src/main/java/ru/geekbrains/team20211224/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/team20211224/MainActivity.kt
@@ -1,11 +1,16 @@
 package ru.geekbrains.team20211224
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var btn: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn = findViewById(R.id.button)
     }
 }
