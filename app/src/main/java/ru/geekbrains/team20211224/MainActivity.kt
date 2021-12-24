af9a4fdc739be0c8c17c7a180772a4157a627e4b
Vladimir Morozov <mrvlmor@gmail.com>
2021-12-24 21:51:14 +0300

    Создал кнопку (#2)
    * Создал кнопку
    
    * Смерджил

--- app/src/main/java/ru/geekbrains/team20211224/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/team20211224/MainActivity.kt
@@ -1,16 +1,19 @@
 package ru.geekbrains.team20211224
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private var button: Button? = null
     private var btn: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn = findViewById(R.id.button)
+
+        button = findViewById(R.id.button)
+        btn = findViewById(R.id.button1)
     }
 }
